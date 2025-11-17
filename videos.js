79eabb4a06de409b4f904476230b46d1f6ecd1ff
kaushalachrekar2-rgb <kaushalachrekar2@gmail.com>
2025-11-17 18:21:45 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -8,16 +8,18 @@ const videos = [
     ],
     "studio": "SOD Create",
     "label": "START",
-    "gallery": "https://javct.net/v/start-406",
-    "trailer": "https://javtrailers.com/video/1start00406",
-    "censoredurl": "https://supjav.com/377429.html",
-    "uncensoredurl": "https://supjav.com/377706.html",
-    "englishurl": "https://javenglish.cc/video/start-406-eng-sub-soccer-players-wife-secretly-fucked-by-brother-in-law-during-tour/",
+    "description": "Minamo, the wife of a popular professional soccer player, lives a fulfilling life in a luxury apartment in Tokyo. However, due to work, her husband is often away from the expeditions of games. Meanwhile, his brother-in-law, a part-time worker who has always lived at home at home, ends up staying there.",
     "ratings": {
       "actress": 10,
       "production": 9,
       "ex": 9,
       "plot": 8
-    }
+    },
+    "gallery": "https://javct.net/v/start-406",
+    "trailer": "https://javtrailers.com/video/1start00406",
+    "censoredurl": "https://supjav.com/377429.html",
+    "uncensoredurl": "https://supjav.com/377706.html",
+    "englishurl": "https://javenglish.cc/video/start-406-eng-sub-soccer-players-wife-secretly-fucked-by-brother-in-law-during-tour/"
   }
 ];
+
