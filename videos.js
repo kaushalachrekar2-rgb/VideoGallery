84abe2f7a14e158b9884b7ae9e1252c367cd55a4
kaushalachrekar2-rgb <kaushalachrekar2@gmail.com>
2025-11-17 20:08:23 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -20,6 +20,252 @@ const videos = [
     "censoredurl": "https://supjav.com/377429.html",
     "uncensoredurl": "https://supjav.com/377706.html",
     "englishurl": "https://javenglish.cc/video/start-406-eng-sub-soccer-players-wife-secretly-fucked-by-brother-in-law-during-tour/"
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "A strong-willed and dedicated female investigator goes undercover with a junior male investigator pretending to be a married couple. They end up living in the room next to Yaza. In order to avoid revealing their true identity and make it seem like they are a real couple, the two live a fake newlywed life. The act, which was supposed to be just for the sake of a mission, eventually sways her heart and makes her fall in love with her junior.",
+    "gallery": null,
+    "trailer": "https://javtrailers.com/video/1start00425",
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "The protagonist is in a long-distance relationship with his older girlfriend Yukari, whom he met online. When he meets for the first time for two days, Yukari is the most erotic and the best girlfriend who has had sex with her many times...Collaborate with a popular doujin work! The original doujin work (A story about how he met a big-breasted girlfriend he met online and was squeezed out).",
+    "gallery": null,
+    "trailer": "https://javtrailers.com/video/1start00383",
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "MINAMO, who has seriously released his sexual desire, takes on the three most intense thumps ever. Her body, thrusts deep inside her vagina, arches and convulses, and climaxes, is soaked with sweat, love juices and drool. Her deep thrusts can't stop, and she screams and screams, covered in body fluids. Eros that surpasses her limits, is now available.",
+    "gallery": null,
+    "trailer": "https://javtrailers.com/video/1start00361",
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "A project for MINAMO fans by MINAMO fans! A carefully selected fan request project from 238 copies is now available! An unprecedented original work has been created! If you catch MINAMO, you can kiss, blowjob, or have sex! MINAMO's sprinting at full speed! The ending is a spectacular 29 shots of real semen splashing!",
+    "gallery": null,
+    "trailer": "https://javtrailers.com/video/1start00346",
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "A juvenile prison that solves \"overwhelming sexual desire caused by adolescence\" and \"frustration caused by prison\" through creampie sex instruction. Elite female guards perform creampie sex with the boys as part of their instruction. Rehabilitation is carried out using a candy and whip ejaculation control program.",
+    "gallery": null,
+    "trailer": "https://javtrailers.com/video/1start00320",
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "MINAMO's 4th anniversary special project is an unprecedented AV drama!? Pink Ranger's sexual desires naturally increase so much that they can't control themselves through the fierce fighting instincts. After the battle, there is a sexual desire processing squad that secretly handles the sexual desires of the heroes. A story about the true story of the Sentai heroes that has never been depicted before.",
+    "gallery": null,
+    "trailer": "https://javtrailers.com/video/1start00302v",
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "A different dimension of time-stop AV x MINAMO bomb! The super popular influencer, Minamo, has gained enormous support from young people. However, one day, he begins to feel uncomfortable with his body, with his pussy smelling like a sperm and his face being dirty. Meanwhile, Minamo is shocked by the video that was played on social media. It was a video of him being stolen by strangers.",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "This is the only place where MINAMO's anus can be seen in close-up shots! The older sister, a cool female college student, has to take care of her at home due to her younger brother's poor health. While being irritated and caring for her, she realizes that her younger brother is a huge dick, and as if to vent her frustration from the date, she squeezes out her brother's semen over and over again in a big ass cowgirl position.",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Minamo"
+    ],
+    "studio": "SOD Create",
+    "label": "START",
+    "description": "When she arrives at the location, she starts sex without notice!? What's more, she has an endless sex with a bead until the night when the filming ends! She must have been confused, but when she inserts, she writhes in the intense thrusting of a big dick! She uses makeup and cooks While doing interviews! Always having sex! A practical AV that's just about to masturbate no matter how many times you cum!",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": "[FNS-132] Kai Masaki Juku. The Awakening Training of the Completely Obscene Woman: 28 Hours with Yoshitaka Nene",
+    "thumbnail": "https://lh3.googleusercontent.com/d/1bk95Wt07JInNCa1uor4vXLRrJNfRV6zj",
+    "coverimage": "https://lh3.googleusercontent.com/d/1t70HtUOHr361DTAdv44Fz7VpJu8eVygQ",
+    "actress": [
+      "Yoshitaka Nene"
+    ],
+    "studio": "Faleno",
+    "label": "FNS",
+    "description": "Breaking down the walls of 8 years as an AV actress!! A two-day, one-night slut awakening training camp where pleasure and domination intersect.1) Slut means motherhood and love 2) It's important to have a spirit of acceptance for each other 3) Being shy is bad 4) Accept everything about the other person 5) Expose your heart 6) Don't ever slack off!! Do your best. A hellish trial to train your hips, voice, physical strength, and movements... A new Yoshitaka slut is born in Spartan special training that overturns all Yoshitaka slut sex up until now!",
+    "gallery": "https://javct.net/v/fns-132",
+    "trailer": "https://javtrailers.com/video/1fns00132",
+    "censoredurl": "https://supjav.com/382209.html",
+    "uncensoredurl": "https://supjav.com/382401.html",
+    "englishurl": "https://javgg.net/jav/fns-132-english-subtitle/"
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Yoshitaka Nene"
+    ],
+    "studio": "Faleno",
+    "label": "FNS",
+    "description": "Yoshitaka Nene's work specializing in maximum facial strength! She has an incredible tongue technique and a vacuum-filled blowjob with her cheeks while being stared at Nene's adorable eyes! She accepts the facial ejaculation of the muddy semen that has accumulated in her face with a smile! From a thrust blowjob that squeezes out to the end even after ejaculation, to a cleaning blowjob! She licks and sucks up her dirty dick with a smile! Yes! The customer is god!",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": "[FNS-089] (4K) My wife, Yoshitaka Nene, was F*rced to perform a shameful naked party trick",
+    "thumbnail": "https://lh3.googleusercontent.com/d/12zITokI0NFR3IIBWTq_7f6dtbh3Dh6Y-",
+    "coverimage": "https://lh3.googleusercontent.com/d/11coZzuO5QkqQmJx0C9XB5aIEkrKWvL8e",
+    "actress": [
+      "Yoshitaka Nene"
+    ],
+    "studio": "Faleno",
+    "label": "FNS",
+    "description": "Nene decides to take part in a company trip with her husband \"family\"... However, it was revealed that Nene was the only family member other than the employee who participated on the trip on the day of the trip. A banquet was held in the evening, and as the wife of a new employee, her boss suggests Nene do a banquet performance. Power harassment pushed her through, and the requests of the employees escalate. At the end, her bosses were in front of her husband's eyes.",
+    "ratings": {
+      "actress": 10,
+      "production": 10,
+      "ex": 10,
+      "plot": 10
+    },
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Yoshitaka Nene"
+    ],
+    "studio": "Faleno",
+    "label": "FNS",
+    "description": "[Licking and eating in the mouth! Secret treatment from a certain dental clinic] At first glance, the beautiful, gentle and serious dental hygienist Nene has a terrible secret face. Saliva and licking her tongue! Saliva for teeth, gum licking, gargling, etc., and using her tongue and chopsticks to her to the fullest, she serves the whole patient's mouth! She is soaked, sucked, siped, and... she is sure to ejaculate in agony. We hope you will enjoy FALENO DENTAL CLINIC, which is recommended for those who hate dentists.",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Yoshitaka Nene"
+    ],
+    "studio": "Faleno",
+    "label": "FNS",
+    "description": "Yoshitaka, a quiet accountant with plain glasses. I thought he was a hard worker who always worked overtime late, but... One night, when I was also working overtime, I tried to call out to Yoshitaka, who was out, and Yoshitaka's PC got cosplay images of her plump butts!? Is this a secret account? The erotic images uploaded every day were Yoshitaka, a plain account! \"Hey, Yoshitaka, right? This is what he said.\" He showed me the butt as a hush-up fee, and then... A big turn of events to a convenient sex friend! A really good ass!",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
+  },
+  {
+    "title": null,
+    "thumbnail": null,
+    "coverimage": null,
+    "actress": [
+      "Yoshitaka Nene"
+    ],
+    "studio": "Faleno",
+    "label": "FNS",
+    "description": "Yoshitaka Nene reverses a man in an office district somewhere in Tokyo and challenges him to kiss him in the city! \"This project is crazy!\" You might be scared, but Onene goes wild! She gets into kissing every single amateur guy! She was hesitant at first, but as he continues to do more and more intense kisses, he gets into a proactive, intense kiss... Eat amateurs with the best kisses!",
+    "gallery": null,
+    "trailer": null,
+    "censoredurl": null,
+    "uncensoredurl": null,
+    "englishurl": null
   }
 ];
 
